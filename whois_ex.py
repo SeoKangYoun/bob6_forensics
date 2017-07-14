cff328e84b49166c4725d53716ae6ff2936f39e6
seokangyoun <tjrkddbs104@naver.com>
2017-07-14 12:08:53 -0400

    whois json

---
+++ whois_ex.py
@@ -0,0 +1,32 @@
+import  whois
+import json
+
+domain_file = open('domains.txt','r')
+ret_file = open('result.txt', 'w')
+
+
+l = domain_file.readline()
+while l:
+
+	ret_file.write("{")
+	domain_info = whois.query(l).__dict__
+	
+	key_list = list(domain_info)
+	value_list = list(domain_info.values())
+	json_val = []
+	for i in range(len(value_list)):
+		json_val.append(repr(key_list[i])+":"+repr(value_list[i]))
+	for item in json_val:
+		if item != json_val[-1]: 
+			ret_file.write(item)
+			ret_file.write(", ")
+		else:
+			ret_file.write(item)
+
+	ret_file.write("}\n")
+
+	l = domain_file.readline()
+
+
+domain_file.close()
+ret_file.close()
